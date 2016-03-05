2ca80b3dcaacff820c86de29ec030b2e879baf46
Cathal Killeen <cathalkilleen@gmail.com>
2016-03-05 18:29:13 +0000

    added marker plugins

--- www/js/controllers/main.js
+++ www/js/controllers/main.js
@@ -1,31 +1,5 @@
 app.controller('MainController', ['$scope', 'leafletData', 'distance', function($scope, leafletData, distance) {
-    var radius = 6371;
-    var pricePerKm = 1.4;
-
-    var toRadians = function(degrees){
-        return ((degrees * Math.PI)/180);
-    }
-
-    var getDistance = function(coords1, coords2){
-        //convert all latlngs to radians
-        lat1 = toRadians(coords1.lat);
-        lng1 = toRadians(coords1.lng);
-        lat2 = toRadians(coords2.lat);
-        lng2 = toRadians(coords2.lng);
-
-        lngDelta = Math.abs(lng2 - lng1);
-
-        //calculate the angle that corresponds to the great circle distance
-        centralAngle = Math.acos((Math.sin(lat1) * Math.sin(lat2)) + (Math.cos(lat1) * Math.cos(lat2) * Math.cos(lngDelta)));
-
-        //get distance segment along great circle
-        distance = radius*centralAngle;
-
-        //reduce to 2 significant figures
-        distance = distance.toFixed(2);
-
-        return Number(distance); //kilometers
-    }
+    var pricePerKm = 0.8;
 
     angular.extend($scope, {
         center: {
@@ -61,8 +35,8 @@ app.controller('MainController', ['$scope', 'leafletData', 'distance', function(
             }
         }
     });
-    $scope.distance = getDistance($scope.markers.pickup, $scope.markers.dropoff);
-    $scope.price = $scope.distance * pricePerKm;
+    $scope.distance = distance.get($scope.markers.pickup, $scope.markers.dropoff);
+    $scope.price = 2 + ($scope.distance * pricePerKm);
 
     $scope.$on('leafletDirectiveMarker.dragend', function(event, args){
         if(args.model.id === 'pu'){
@@ -72,7 +46,7 @@ app.controller('MainController', ['$scope', 'leafletData', 'distance', function(
         }
         console.log($scope.markers)
 
-        $scope.distance = getDistance($scope.markers.pickup, $scope.markers.dropoff);
-        $scope.price = $scope.distance * pricePerKm;
+        $scope.distance = distance.get($scope.markers.pickup, $scope.markers.dropoff);
+        $scope.price = 2 + ($scope.distance * pricePerKm);
     })
 }])
